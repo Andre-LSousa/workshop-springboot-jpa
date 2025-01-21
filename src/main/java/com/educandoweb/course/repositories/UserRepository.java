635b01fe1bd5cc892700d77be332f7cbc60b40ce
Andre Luiz <andresanteria@hotmail.com>
2025-01-21 15:38:27 -0300

    Order. Instant, ISO 8601 part - 2

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -1,8 +1,9 @@
-package com.educandoweb.course.resources.repositories;
+package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educandoweb.course.entities.User;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
